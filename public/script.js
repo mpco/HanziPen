6d4c648040c97cf766e4418898071a089a68e1e6
mpco <aizaitianwai@gmail.com>
2024-04-05 17:04:52 +0800

    增加显示拼音

--- public/script.js
+++ public/script.js
@@ -24,6 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
     cell.addEventListener('click', function () {
       if (cell.textContent.match(/^[\u4e00-\u9fa5]+$/)) {
         displayCharacterAnimation(cell.textContent);
+        showPinyin(cell.textContent);
       }
     });
     textGrid.appendChild(cell);
@@ -40,7 +41,9 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
+  // 重置笔画窗格
   function resetCharacterDisplay() {
+    showPinyin('字')
     characterDisplay.innerHTML = '';
     HanziWriter.create(characterDisplay, '字', {
       width: 300,
@@ -49,7 +52,23 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
+  // 显示拼音
+  function showPinyin(char) {
+    var { pinyin } = pinyinPro;
+    const pinyin_initial = pinyin(char, { pattern: 'initial' });
+    const pinyin_initial_char = pinyin(char, { toneType: 'none', pattern: 'initial' });
+    const pinyin_final = pinyin(char, { pattern: 'final' });
+    const pinyin_final_char = pinyin(char, { toneType: 'none', pattern: 'final' });
+
+    document.getElementById('pinyin_initial').innerText = pinyin_initial;
+    document.getElementById('pinyin_initial_upper').innerText = pinyin_initial_char.toUpperCase();
+    document.getElementById('pinyin_final').innerText = pinyin_final;
+    document.getElementById('pinyin_final_upper').innerText = pinyin_final_char.toUpperCase();
+    
+  }
+
   resetCharacterDisplay();
+
   // 显示汉字笔画动画的函数
   function displayCharacterAnimation(char) {
 
@@ -71,5 +90,8 @@ document.addEventListener('DOMContentLoaded', function () {
 // Function to speak the character using Web Speech API
 function speakCharacter(character) {
   const utterance = new SpeechSynthesisUtterance(character);
+  utterance.lang = 'zh-CN';
+  // utterance.voice = speechSynthesis.getVoices()[0];
+  utterance.rate = 0.1;
   speechSynthesis.speak(utterance);
 }
